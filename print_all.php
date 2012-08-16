6d4e0cfa37c43fa38bbdc65706516b864c9e9b40
Marko <marko@giscloud.com>
2012-08-17 00:25:18 +0200

    sorted out page listing order

--- print_all.php
+++ print_all.php
@@ -8,39 +8,55 @@
 
 <?php
 
-$args = array(
-    'post_type' => array( 'page' ),
-    'post_status' => array( 'publish' ),
-    'orderby' => 'parent',
-    'order' => 'asc',
-    'posts_per_page' => 900,
-    'post__not_in' => array( get_the_ID() )
-    );
-query_posts( $args );
+
+function listPages($parent_id)
+{
+    $args = array(
+        'post_type' => array( 'page' ),
+        'post_status' => array( 'publish' ),
+        'orderby' => 'menu_order',
+        'order' => 'asc',
+        'post_parent' => $parent_id,
+        'posts_per_page' => 900,
+        'post__not_in' => array( get_the_ID() )
+        );
+    query_posts( $args );
+
+    $args=array('child_of' => $parent_id, 'parent' => $parent_id, 'sort_column'=>'menu_order', 'exclude'=>get_the_ID());
+    $pages = get_pages($args);
+
+    if ($pages)
+    {
+
+        foreach ($pages as $page)
+        {
+            echo '<div class="page" style="margin-bottom:40px;" >';
+            echo '    <div class="page-header">';
+            echo '        <h1 class="postTitle">'.$page->post_title.'</h1>';
+            echo '    </div>';
+            echo '    <div class="" >';
+            echo $page->post_content;
+            echo '    </div>';
+            echo '</div>';
+
+            listPages($page->ID);
+        }
+
+
+    }
+
+} ?>
+
+<?php
+echo '<div id="content" class="'.CONTAINER_CLASSES.'">';
+echo '    <div id="main" class="'.MAIN_CLASSES.'" role="main">';
 ?>
 
-<div id="content" class="<?php echo CONTAINER_CLASSES; ?>">
+<?php listPages(0); ?>
 
-    <div id="main" class="<?php echo MAIN_CLASSES; ?>" role="main">
-
-        <? if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-        <div class="page" style="margin-bottom:40px;" >
-            <div class="page-header">
-                <h1 class="postTitle"><?php the_title(); ?></h1>
-            </div>
-            <div class="" >
-                <?php the_content(__('(more...)')); ?>
-            </div>
-        </div>
-
-        <?php endwhile; else: ?>
-
-        <p>Sorry, no pages matched your criteria.</p>
-
-        <?php endif; ?>
-
-    </div>
-</div>
+<?php
+echo '    </div>';
+echo '</div>';
+?>
 
 <?php get_footer(); ?>
